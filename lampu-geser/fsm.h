89e475ff0f4ce14c90e661a50c2c972d2d707dc7
waskita <waskita@ee.itb.ac.id>
2025-05-07 17:00:00 +0700

    tambah FSM
    tambah FSM

--- lampu-geser/fsm.h
+++ lampu-geser/fsm.h
@@ -1,11 +1,18 @@
-#define FSM_GESER_STATE_S1 1
-#define FSM_GESER_STATE_S2 2
-#define FSM_GESER_STATE_S3 3
-#define FSM_GESER_STATE_S4 4
+#define FSM_DEBOUNCE_STATE_0 100
+#define FSM_DEBOUNCE_STATE_1 101
 
-#define FSM_EDGE_STATE_0 100
-#define FSM_EDGE_STATE_1 101
+#define FSM_EDGE_STATE_0 200
+#define FSM_EDGE_STATE_1 201
 
+#define FSM_GESER_STATE_S1 301
+#define FSM_GESER_STATE_S2 302
+#define FSM_GESER_STATE_S3 303
+#define FSM_GESER_STATE_S4 304
+
+void FSM_edge_detect_init(int *state, int *output);
+void FSM_edge_detect(int input, int *state, int *output);
 
 void FSM_geser_init(int *state, int *output);
 void FSM_geser(int input_kiri, int input_kanan, int *state, int *output);
+
+
--- lampu-geser/main.c
+++ lampu-geser/main.c
@@ -2,14 +2,113 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include "fsm.h"
 
 int main()
 {
     printf("%s\n",__FILE__);
+    uji_fsm_debounce();
     uji_fsm_geser();
+    uji_fsm_edge_detect();
     return 0;
 }
 
+uji_fsm_debounce()
+{
+    char output_filename[]="debounce.csv";
+
+    FILE *out_f ;
+
+    int state;
+    int output;
+    int input;
+    int counter;
+    int delay=10; // durasi delay untuk debounce
+
+    int k=0; // simulation time
+    out_f =fopen(output_filename, "w");
+    fprintf(out_f,"k,input,state,counter,output\n");
+
+    FSM_debounce_init(&state,&counter,&output);
+    input=0;
+
+    for(int k=0; k<50; k++)
+    {
+        input=0; // default
+
+        if(k==5)  // bikin bounce bbrp
+        {
+            input=1;
+        }
+
+        if(k==7)  // bikin bounce bbrp
+        {
+            input=1;
+        }
+
+
+        if(k>8 && k<20)
+        {
+            input=1;
+        }
+// #bounce again
+
+        if(k==22)  // bikin bounce bbrp
+        {
+            input=1;
+        }
+        if(k==25)  // bikin bounce bbrp
+        {
+            input=1;
+        }
+        if(k>40)
+        {
+            input=1;
+        }
+
+        FSM_debounce(input,&state,&counter,&output,delay);
+        fprintf(out_f,"%d,%d,%d,%d,%d\n", k,input,state,counter,output);
+    }
+}
+
+void uji_fsm_edge_detect()
+{
+    char output_filename[]="edge.csv";
+
+    FILE *out_f ;
+    int edge_state;
+    int edge_output;
+    int edge_input;
+
+    int k=0; // simulation time
+    out_f =fopen(output_filename, "w");
+    fprintf(out_f,"k,edge_input,edge_state,edge_output\n");
+
+    FSM_edge_detect_init(&edge_state,&edge_output);
+    edge_input=0;
+
+    for(int k=0; k<30; k++)
+    {
+        edge_input=0; // default
+
+        if(k>3 && k<7)
+        {
+            edge_input=1;
+        }
+
+        if(k>11 && k<15)
+        {
+            edge_input=1;
+        }
+        if(k>18 && k<23)
+        {
+            edge_input=1;
+        }
+
+        FSM_edge_detect(edge_input,&edge_state,&edge_output);
+        fprintf(out_f,"%d,%d,%d,%d\n", k,edge_input,edge_state,edge_output);
+    }
+}
 
 void uji_fsm_geser()
 {
@@ -78,5 +177,4 @@ void uji_fsm_geser()
         FSM_geser(geser_input_kiri,geser_input_kanan,&geser_state,&geser_output);
         fprintf(out_f,"%d,%d,%d,%d,%d\n", k,geser_input_kiri,geser_input_kanan,geser_state,geser_output);
     }
-
 }
